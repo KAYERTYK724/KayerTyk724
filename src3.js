71d366bb17e091074b15244f1209ee20775144ea
KAYERTYK724 <155451281+KAYERTYK724@users.noreply.github.com>
2024-01-10 22:27:10 +0700

    Add files via upload

---
+++ src3.js
@@ -0,0 +1,16 @@
+//
+let name = "Trương Hoàng Diễm Ái";
+let app = document.getElementById("PC05853");
+// arrow function
+let baiTap = function(){
+    console.log(`${name}, ${app}`);
+}
+//
+baiTap();
+// 
+const arr1 = ['JAN', 'FEB' ,'MAR', 'APR', 'MAY'];
+let arr2;
+// 
+arr2 = [...arr1];
+//
+console.log(arr2);
